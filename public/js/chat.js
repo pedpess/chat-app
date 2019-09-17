87a78c5380c70535d21c12b01fc5fd0aed73fb1b
Pedro Pessoa <pedro.pessoa@starship.co>
2019-09-17 20:37:30 +0300

    Generate the time messages were sent

--- public/js/chat.js
+++ public/js/chat.js
@@ -16,15 +16,16 @@ const locationMessageTemplate = document.querySelector(
 socket.on("message", message => {
   console.log(message);
   const html = Mustache.render(messageTemplate, {
-    message
+    message: message.text,
+    createdAt: moment(message.createdAt).format("h:mm a")
   });
   $messages.insertAdjacentHTML("beforeend", html);
 });
 
-socket.on("locationMessage", url => {
-  console.log(url);
+socket.on("locationMessage", message => {
   const html = Mustache.render(locationMessageTemplate, {
-    url
+    url: message.url,
+    createdAt: moment(message.createdAt).format("h:mm a")
   });
 
   $messages.insertAdjacentHTML("beforeend", html);
