2d2e2726af42694727a9bf6777058591659cb6c3
Pedro Pessoa <pedro.pessoa@starship.co>
2019-09-22 15:37:29 +0300

    Add sidebar template to render users in a room

--- public/js/chat.js
+++ public/js/chat.js
@@ -12,6 +12,7 @@ const messageTemplate = document.querySelector("#message-template").innerHTML;
 const locationMessageTemplate = document.querySelector(
   "#location-message-template"
 ).innerHTML;
+const sidebarTemplate = document.querySelector("#sidebar-template").innerHTML;
 
 // Options
 const { username, room } = Qs.parse(location.search, {
@@ -36,6 +37,15 @@ socket.on("locationMessage", message => {
   });
 
   $messages.insertAdjacentHTML("beforeend", html);
+});
+
+socket.on("roomData", ({ room, users }) => {
+  const html = Mustache.render(sidebarTemplate, {
+    room,
+    users
+  });
+
+  document.querySelector('#sidebar').innerHTML = html;
 });
 
 $messageForm.addEventListener("submit", e => {
--- src/index.js
+++ src/index.js
@@ -40,6 +40,11 @@ io.on("connection", socket => {
       .to(user.room)
       .emit("message", generateMessage(`${user.username} has joined!`));
 
+    io.to(user.room).emit("roomData", {
+      room: user.room,
+      users: getUsersInRoom(user.room)
+    });
+
     callback();
   });
 
@@ -75,6 +80,10 @@ io.on("connection", socket => {
         "message",
         generateMessage(`${user.username} has left!`)
       );
+      io.to(user.room).emit("roomData", {
+        room: user.room,
+        users: getUsersInRoom(user.room)
+      });
     }
   });
 });
